b811c17c1ce80dec517945eef5eeb039d84e2c24
Pragya Bisen <pragyabisen14@gmail.com>
2025-05-07 00:39:57 +0530

    task1: project bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        Your  calculator will display here!
+      </div>
+    </>
+  )
+}
+
+export default App
